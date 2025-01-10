ab3425056e0feae8c60198eabe866b9ae5bda599
尤雨东 <root@ltpp.vip>
2025-01-10 23:09:13 +0800

    feat:v0.29.1

--- src/log/thread.rs
+++ src/log/thread.rs
@@ -1,35 +1,22 @@
-use crate::{Log, LogArcLock};
-use lazy_static::lazy_static;
+use crate::Log;
 use recoverable_spawn::*;
-use std::sync::{Arc, RwLock};
-
-lazy_static! {
-    static ref LOG: LogArcLock = Arc::new(RwLock::new(Log::default()));
-}
+use std::sync::Arc;
 
 #[inline]
 pub fn log_run(log: &Log) -> JoinHandle<()> {
-    let log_clone: Log = log.clone();
-    {
-        if let Ok(mut log) = LOG.write() {
-            *log = log_clone;
-        }
-    }
+    let arc_log: Arc<Log> = Arc::new(log.clone());
     let log_thread: JoinHandle<()> = recoverable_spawn(move || {
+        let arc_error_log_clone: Arc<Log> = Arc::clone(&arc_log);
+        let arc_info_log_clone: Arc<Log> = Arc::clone(&arc_log);
+        let arc_debug_log_clone: Arc<Log> = Arc::clone(&arc_log);
         let error_log_thread: JoinHandle<()> = recoverable_spawn(move || loop {
-            if let Ok(log) = LOG.read() {
-                log.write_error();
-            }
+            arc_error_log_clone.write_error();
         });
         let info_log_thread: JoinHandle<()> = recoverable_spawn(move || loop {
-            if let Ok(log) = LOG.read() {
-                log.write_info();
-            }
+            arc_info_log_clone.write_info();
         });
         let debug_log_thread: JoinHandle<()> = recoverable_spawn(move || loop {
-            if let Ok(log) = LOG.read() {
-                log.write_debug();
-            }
+            arc_debug_log_clone.write_debug();
         });
         let _ = error_log_thread.join();
         let _ = info_log_thread.join();
