8589e2548d9998b8951fd196ca326bf68a43497e
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:35 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/hyperlane-log.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/hyperlane-log.git;
 git remote add origin git@github.com:ltpp-universe/hyperlane-log.git;
