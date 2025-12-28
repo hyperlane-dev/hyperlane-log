54f726761e0b142e9db4d80101e9ec692044be52
尤雨东 <root@ltpp.vip>
2025-12-28 11:22:52 +0800

    feat: v1.19.7

--- src/cfg.rs
+++ src/cfg.rs
@@ -5,34 +5,34 @@ async fn test() {
     let log: Log = Log::new("./logs", 1_024_000);
     let error_str: String = String::from("custom error message");
     log.error(error_str, |error| {
-        let write_data: String = format!("User error func => {:?}\n", error);
+        let write_data: String = format!("User error func => {error:?}\n");
         write_data
     });
     let info_str: String = String::from("custom info message");
     log.info(info_str, |info| {
-        let write_data: String = format!("User info func => {:?}\n", info);
+        let write_data: String = format!("User info func => {info:?}\n");
         write_data
     });
     let debug_str: String = String::from("custom debug message");
     log.debug(debug_str, |debug| {
-        let write_data: String = format!("User debug func => {:#?}\n", debug);
+        let write_data: String = format!("User debug func => {debug:#?}\n");
         write_data
     });
     let async_error_str: String = String::from("custom async error message");
     log.async_error(async_error_str, |error| {
-        let write_data: String = format!("User error func => {:?}\n", error);
+        let write_data: String = format!("User error func => {error:?}\n");
         write_data
     })
     .await;
     let async_info_str: String = String::from("custom async info message");
     log.async_info(async_info_str, |info| {
-        let write_data: String = format!("User info func => {:?}\n", info);
+        let write_data: String = format!("User info func => {info:?}\n");
         write_data
     })
     .await;
     let async_debug_str: String = String::from("custom async debug message");
     log.async_debug(async_debug_str, |debug| {
-        let write_data: String = format!("User debug func => {:#?}\n", debug);
+        let write_data: String = format!("User debug func => {debug:#?}\n");
         write_data
     })
     .await;
@@ -44,29 +44,29 @@ async fn test_more_log_first() {
     use crate::*;
     let log: Log = Log::new("./logs", DISABLE_LOG_FILE_SIZE);
     log.error("error data => ", |error| {
-        let write_data: String = format!("User error func => {:?}\n", error);
+        let write_data: String = format!("User error func => {error:?}\n");
         write_data
     });
     log.info("info data => ", |info| {
-        let write_data: String = format!("User info func => {:?}\n", info);
+        let write_data: String = format!("User info func => {info:?}\n");
         write_data
     });
     log.debug("debug data => ", |debug| {
-        let write_data: String = format!("User debug func => {:#?}\n", debug);
+        let write_data: String = format!("User debug func => {debug:#?}\n");
         write_data
     });
     log.async_error("async error data => ", |error| {
-        let write_data: String = format!("User error func => {:?}\n", error);
+        let write_data: String = format!("User error func => {error:?}\n");
         write_data
     })
     .await;
     log.async_info("async info data => ", |info| {
-        let write_data: String = format!("User info func => {:?}\n", info);
+        let write_data: String = format!("User info func => {info:?}\n");
         write_data
     })
     .await;
     log.async_debug("async debug data => ", |debug| {
-        let write_data: String = format!("User debug func => {:#?}\n", debug);
+        let write_data: String = format!("User debug func => {debug:#?}\n");
         write_data
     })
     .await;
--- src/log/type.rs
+++ src/log/type.rs
@@ -2,13 +2,18 @@ use crate::*;
 
 /// A collection of named log formatting functions.
 pub type ListLog<T> = Vec<(String, ArcLogFunc<T>)>;
+
 /// Thread-safe shared reference to a collection of log functions.
 pub type LogListArcLock<T> = Arc<RwLock<ListLog<T>>>;
+
 /// Thread-safe shared reference to a Log configuration instance.
 pub type LogArcLock = Arc<RwLock<Log>>;
+
 /// Trait object representing a log formatting function.
 pub type LogFunc<T> = dyn LogFuncTrait<T>;
+
 /// Thread-safe shared reference to a log formatting function.
 pub type ArcLogFunc<T> = Arc<LogFunc<T>>;
+
 /// Thread-safe shared reference to a Log configuration.
 pub type ArcLog = Arc<Log>;
